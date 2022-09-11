eb099e6067cc18daa38ebba83f1e2eefca4dfecc
Patidifusa <zs20006765@estudiantes.uv.mx>
2022-09-11 07:16:46 -0500

    create work experience #3

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperienceComponent implements OnInit {
 
+  workExperience : Array<any> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    let work1 = {
+      fecha: "2019",
+      ubicacion: "Potrerillo, Ver",
+      puesto: "Soporte técnico",
+      empresa:"Escuela Kohkonetzin",
+      logros : [
+          { descripcion: "Instalación de Software en las computadoras"},
+          { descripcion: "Aplicación web utilizando HTML con conexión MySQL y XAMPP"},
+          { descripcion: "Mantenimiento y soporte de computadoras"}
+
+      ]
+    };
+
+    this.workExperience.push(work1);
+
   }
 
 }
